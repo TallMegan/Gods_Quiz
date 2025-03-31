27fdf3a0651f1c9b91a4b34e6c9ea36ed6f2bd34
Terrence <135181592+TallMegan@users.noreply.github.com>
2025-03-31 14:50:47 +1300

    Retrieves the gods and their duties from the csv file

---
+++ C_03_retrieve_csv.py
@@ -0,0 +1,23 @@
+import csv
+
+
+def get_gods():
+    """
+    Retrieves the gods and duties from a csv file
+    and puts them into a list
+    """
+
+    # retrieve colours from csv file and put them in a list
+    file = open("gods.csv", "r")
+    all_gods = list(csv.reader(file, delimiter=","))
+    file.close()
+
+    # remove the first row
+    all_gods.pop(0)
+
+    return all_gods
+
+
+gods = get_gods()
+
+print(gods)
