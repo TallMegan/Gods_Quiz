059cf62469af868c3279ada1b4827e5f2c5dfbd6
Terrence <135181592+TallMegan@users.noreply.github.com>
2025-04-02 10:03:10 +1300

    In process of making sure no duplicates are selected

--- C_03_retrieve_csv.py
+++ C_03_retrieve_csv.py
@@ -2,23 +2,43 @@ import csv
 import random
 
 
-def get_gods():
-    """
-    Retrieves the gods and duties from a csv file
-    and puts them into a list
-    """
+# def get_gods():
+#     """
+#     Retrieves the gods and duties from a csv file
+#     and puts them into a list
+#     """
+#
+#     # retrieves gods from csv file and put them in a list
+#     file = open("gods.csv", "r")
+#     all_gods = list(csv.reader(file, delimiter=","))
+#     file.close()
+#
+#     # remove the first row
+#     all_gods.pop(0)
+#
+#     return all_gods
+#
+#
+# gods = get_gods()
 
-    # retrieves gods from csv file and put them in a list
-    file = open("gods.csv", "r")
-    all_gods = list(csv.reader(file, delimiter=","))
-    file.close()
+gods = [["Zeus", "Lightning"], ["Poseidon", "Ocean"], ["Hades", "Underworld"]]
 
-    # remove the first row
-    all_gods.pop(0)
+for item in range(0, 3):
+    god_selected = random.choice(gods)
+    god_name = god_selected[0]
+    correct_duty = god_selected[1]
 
-    return all_gods
+    incorrect_duties = []
+    cycles = 0
+    while cycles < 2:
+        random_duties = random.choice(gods)
+        if random_duties[1] not in incorrect_duties:
+            incorrect_duties.append(random_duties[1])
+            cycles += 1
+        else:
+            continue
 
-
-gods = get_gods()
-
-print(gods)
+    print(f"God Selected: {god_name}"
+          f"\nCorrect Duty: {correct_duty}"
+          f"\nIncorrect Duty 1: {incorrect_duties[0]}"
+          f"\nIncorrect Duty 2: {incorrect_duties[1]}\n")
